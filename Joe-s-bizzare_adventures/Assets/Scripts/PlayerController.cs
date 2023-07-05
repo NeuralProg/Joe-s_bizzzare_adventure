b6aaffd4913ffff00e08c068673eefe9f9295f5d
NeuralPrg <chabaneyann@gmail.com>
2023-07-05 19:45:46 +0200

    jump and tiles

--- Joe-s-bizzare_adventures/Assets/Scripts/PlayerController.cs
+++ Joe-s-bizzare_adventures/Assets/Scripts/PlayerController.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 
 /*
     - Fonctions et #regions
-    - Jump anim --> animator Trigger
     - 
 */
 
 public class PlayerController : MonoBehaviour
 {
+    #region Variables
+
     // X Move
     private float moveSpeed = 5f;
 
     [Header("Jump")]
     [SerializeField] private GameObject doubleJumpEffect;
     private bool jumping = false;
-    private float jumpHeight = 20f;
+    private float jumpHeight = 18f;
     private float jumpCoyoteTime = 0.2f;
     private float jumpCoyoteTimer;
     private bool canDoubleJump;
@@ -32,6 +33,10 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
 
+    #endregion
+
+
+    #region Default
 
     void Start()
     {
@@ -41,12 +46,63 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        MoveHorizontaly();
+        Jump();
+        
+        // Checks
+        isGrounded = Physics2D.OverlapCircle(groundCheckPos.position, 0.15f, whatIsGround);
+        isFalling = rb.velocity.y < -0.1f;
+        if(isGrounded)
+        {
+            canDoubleJump = true;
+            if(!jumping)
+                jumpCoyoteTimer = jumpCoyoteTime;
+        }
+        else
+        {
+            jumpCoyoteTimer -= Time.deltaTime;
+        }
+        if (isFalling) 
+        {
+            jumping = false;
+        }
+
+        // Limit falling speed
+        rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -30, 30));
+
+        // Anims
+        anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+        anim.SetBool("Grounded", isGrounded);
+        anim.SetBool("Jumping", jumping);
+        anim.SetBool("Falling", isFalling);
+    }
+
+    #endregion
+
+
+    #region Functions
+
+    private void MoveHorizontaly()
+    {
         // x movement 
         var xDirection = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(xDirection * moveSpeed, rb.velocity.y);
 
+        // flip
+        if (rb.velocity.x < -0.1f)
+        {
+            transform.localScale = new Vector3(-1f, 1, 1);
+        }
+        else if (rb.velocity.x > 0.1f)
+        {
+            transform.localScale = Vector3.one;
+        }
+    }
+
+    private void Jump()
+    {
         // Jump
-        if((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Z)) && (isGrounded || jumpCoyoteTimer > 0))
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Z)) && (isGrounded || jumpCoyoteTimer > 0))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
             jumping = true;
@@ -66,42 +122,7 @@ public class PlayerController : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y / 3);
             jumping = false;
         }
-
-        // Checks
-        isGrounded = Physics2D.OverlapCircle(groundCheckPos.position, 0.15f, whatIsGround);
-        isFalling = rb.velocity.y < -0.1f;
-        if(isGrounded)
-        {
-            canDoubleJump = true;
-            if(!jumping)
-                jumpCoyoteTimer = jumpCoyoteTime;
-        }
-        else
-        {
-            jumpCoyoteTimer -= Time.deltaTime;
-        }
-        if (isFalling) 
-        {
-            jumping = false;
-        }
-
-        // flip
-        if (rb.velocity.x < -0.1f)
-        {
-            transform.localScale = new Vector3(-1f, 1, 1);
-        }
-        else if(rb.velocity.x > 0.1f)
-        {
-            transform.localScale = Vector3.one;
-        }
-
-        // Limit falling speed
-        rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -30, 30));
-
-        // Anims
-        anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
-        anim.SetBool("Grounded", isGrounded);
-        anim.SetBool("Jumping", jumping);
-        anim.SetBool("Falling", isFalling);
     }
+
+    #endregion
 }
